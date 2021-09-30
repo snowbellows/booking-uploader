1c337465436d3752a4248e96f9f42128ab1b9c0f
Alex Halverson <alexhalverson@posteo.net>
2021-09-30 21:57:51 +1000

    feat(csv): added validation for time and userId

--- src/utils/parseBookings.ts
+++ src/utils/parseBookings.ts
@@ -1,5 +1,6 @@
 import { ok, err, Result } from 'neverthrow';
 import { ParseError } from 'papaparse';
+import { DateTime } from 'luxon';
 
 import { Booking, isBooking, keys } from './booking';
 import { parseFile } from './csv';
@@ -18,6 +19,16 @@ export function parseBookings(
         transform: transformDuration,
         message: (value: string) =>
           `Expected an integer for field "duration", but parsed "${value}"`,
+      },
+      time: {
+        transform: transformTime,
+        message: (value: string) =>
+          `Expected a date time in specified format for field "time", but parsed "${value}"`,
+      },
+      userId: {
+        transform: transformUserId,
+        message: (value: string) =>
+          `Expected a string of 4 numbers like "0001" for field "userId", but parsed "${value}"`,
       },
     },
   };
@@ -39,4 +50,26 @@ function transformDuration(value: string): Result<number, undefined> {
   }
 
   return ok(parsedDuration);
+}
+
+function transformTime(value: string): Result<string, undefined> {
+  const parsedTime = DateTime.fromFormat(
+    value,
+    "dd MMM yyyy HH:mm:ss 'GMT'ZZZ"
+  );
+
+  if (!parsedTime.isValid) {
+    return err(undefined);
+  }
+  return ok(value);
+}
+
+function transformUserId(value: string): Result<string, undefined> {
+  const userIdRegex = /^[0-9]{4}$/;
+
+  if (!userIdRegex.test(value)) {
+    return err(undefined);
+  }
+
+  return ok(value);
 }
