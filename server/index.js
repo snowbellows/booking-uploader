be831c6eb6bfddc3f9e70bb807f39ffef530721d
Alex Halverson <alexhalverson@posteo.net>
2021-10-01 19:31:38 +1000

    feat(server): add bookings post functionality

--- server/index.js
+++ server/index.js
@@ -1,20 +1,65 @@
-const express = require('express')
-const cors = require('cors')
-const fs = require('fs')
+const express = require('express');
+const cors = require('cors');
+var bodyParser = require('body-parser');
+const fs = require('fs');
+const { DateTime } = require('luxon');
 
-const app = express()
-app.use(cors()) // so that app can access
+const { validateBookings, validateBookingsBulk } = require('./validators');
 
-const bookings = JSON.parse(fs.readFileSync('./server/bookings.json')).map(
-  (bookingRecord) => ({
-    time: Date.parse(bookingRecord.time),
-    duration: bookingRecord.duration * 60 * 1000, // mins into ms
-    userId: bookingRecord.user_id,
-  }),
-)
+const dateTimeFormat = "dd MMM yyyy HH:mm:ss 'GMT'ZZZ";
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors()); // so that app can access
+
+let bookings = JSON.parse(fs.readFileSync('./bookings.json')).map(
+  (bookingRecord) => {
+    return {
+      time: DateTime.fromFormat(bookingRecord.time, dateTimeFormat).toUTC(),
+      duration: bookingRecord.duration * 60 * 1000, // mins into ms
+      userId: bookingRecord.user_id,
+    };
+  }
+);
 
 app.get('/bookings', (_, res) => {
-  res.json(bookings)
-})
+  res.setHeader('Content-Type', 'application/json');
+  res.json(bookings);
+});
 
-app.listen(3001)
+app.post('/bookings', (req, res) => {
+  const { body } = req;
+  console.log(body);
+  const validationResult = validateBookings(body);
+
+  if (validationResult.isErr()) {
+    const err = validationResult.error;
+    res.setHeader('Content-Type', 'application/json');
+    res
+      .status(err.code)
+      .json({ errorType: err.type, errorMessage: err.message });
+  } else {
+    bookings = [...bookings, validationResult.value];
+  }
+  res.sendStatus(200);
+});
+
+app.post('/bookings/bulk', (req, res) => {
+  const { body } = req;
+  console.log(body);
+
+  const validationResult = validateBookingsBulk(body);
+
+  if (validationResult.isErr()) {
+    const err = validationResult.error;
+    res.setHeader('Content-Type', 'application/json');
+    res
+      .status(err.code)
+      .json({ errorType: err.type, errorMessage: err.message });
+  } else {
+    bookings = [...bookings, ...validationResult.value];
+  }
+  res.sendStatus(200);
+});
+
+app.listen(3001);
---
+++ server/validators.js
@@ -0,0 +1,80 @@
+const { ok, err, combine } = require('neverthrow');
+const { DateTime } = require('luxon');
+
+const bookingKeys = ['time', 'duration', 'userId'];
+const dateTimeFormat = "dd MMM yyyy HH:mm:ss 'GMT'ZZZ";
+
+function validateBookings(body) {
+  if (typeof body !== 'object') {
+    return err({
+      code: '400',
+      type: 'malformed-body',
+      message: 'Body should be a single booking as an object',
+    });
+  }
+  return validateBooking(body);
+}
+
+function validateBookingsBulk(body) {
+  if (!Array.isArray(body)) {
+    return err({
+      code: '400',
+      type: 'malformed-body',
+      message: 'Body should be an array of bookings',
+    });
+  }
+  return combine(body.map(validateBooking));
+}
+
+function validateBooking(booking) {
+  const fieldsToCheck = Object.keys(booking);
+  const keysMatch =
+    fieldsToCheck.reduce((acc, key) => {
+      return bookingKeys.includes(key) && acc;
+    }, true) && fieldsToCheck.length === bookingKeys.length;
+
+  if (!keysMatch) {
+    return err({
+      code: '400',
+      type: 'malformed-body',
+      message: `Booking keys should be ${bookingKeys.join(', ')}`,
+    });
+  }
+
+  const parsedTime = DateTime.fromFormat(booking.time, dateTimeFormat);
+
+  if (!parsedTime.isValid) {
+    return err({
+      code: '400',
+      type: 'malformed-body',
+      message: `Time values should be in the format "${dateTimeFormat}"`,
+    });
+  }
+
+  const parsedDuration = parseInt(booking.duration);
+
+  if (isNaN(parsedDuration)) {
+    return err({
+      code: '400',
+      type: 'malformed-body',
+      message: `Duration values should be integers`,
+    });
+  }
+  const userIdRegex = /^[0-9]{4}$/;
+
+  if (typeof booking.userId !== 'string' || !userIdRegex.test(booking.userId)) {
+    return err({
+      code: '400',
+      type: 'malformed-body',
+      message: `userId values should be strings of four digits`,
+    });
+  }
+
+  return ok({
+    time: parsedTime,
+    duration: parsedDuration * 60 * 1000, // mins into ms
+    userId: booking.userId,
+  });
+}
+
+module.exports = { validateBookings, validateBookingsBulk };
